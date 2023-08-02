28f94aca75b0a380795bcad95e98a332b019520a
G-Alessandro <galloalessandro77@gmail.com>
2023-08-02 17:51:39 +0200

    Added function prepend and size inside LinkedList

--- src/linked-lists.js
+++ src/linked-lists.js
@@ -13,6 +13,7 @@ class LinkedList {
     this.tail = null;
   }
 
+  // Add a Node
   append(value) {
     const newNode = new Node(value);
 
@@ -25,10 +26,31 @@ class LinkedList {
     }
   }
 
-  // prepend(value) {
+  // Adds a node to the beginning of the linked-list
+  prepend(value) {
+    const newNode = new Node(value);
 
-  // }
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+  }
 
+  // Show the size of the list
+  size() {
+    let length = 0;
+    let currentNode = this.head;
+    while (currentNode) {
+      length += 1;
+      currentNode = currentNode.next;
+    }
+    console.log(length);
+  }
+
+  // Show the complete list
   toString() {
     const linkedListArr = [];
     let currentNode = this.head;
@@ -44,7 +66,8 @@ const linkedList = new LinkedList();
 linkedList.append(10);
 linkedList.append(20);
 linkedList.append(30);
-
+linkedList.prepend(60);
+linkedList.size();
 linkedList.toString();
 
 console.log('test');
